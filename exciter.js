229a574359d16b0671bc4c1e0603b6f1455581f3
jerrytechlimited <jerrytechlimited@gmail.com>
2025-09-20 11:17:51 +0100

    Update exciter.js

--- exciter.js
+++ exciter.js
@@ -576,7 +576,7 @@ function startPaystack() {
     document.getElementById('payModal2').style.display = '';
     return;
   }
-  let amount = 5000 * 1000 // Amount in kobo (₦5000)
+  let amount = 5000 * 100 // Amount in kobo (₦5000)
   let handler = PaystackPop.setup({
     key: 'pk_test_7051b3225597c778f3523710e74ac5da75022fe2',
     email: email,
