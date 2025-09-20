e314ee5977c7dc2092970778e99ded04e23783d6
jerrytechlimited <jerrytechlimited@gmail.com>
2025-09-20 09:44:05 +0100

    Update playaudiomix.js

--- playaudiomix.js
+++ playaudiomix.js
@@ -576,9 +576,9 @@ function startPaystack() {
     document.getElementById('payModal2').style.display = '';
     return;
   }
-  let amount = 1000 * 100; // Amount in kobo (₦1000)
+  let amount = 5000 * 100; // Amount in kobo (₦5000)
   let handler = PaystackPop.setup({
-    key: 'pk_live_656d3f492c531cc4599abaa10d424d6ac8313954',
+    key: 'pk_test_7051b3225597c778f3523710e74ac5da75022fe2',
     email: email,
     amount: amount,
     currency: "NGN",
